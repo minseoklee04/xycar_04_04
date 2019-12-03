6ec7d27bbe85a70ae968deebd09733c673971fa6
SeokHyeon-Eom <eom224@naver.com>
2019-12-04 01:33:14 +0900

    ghrtl

---
+++ assn2/12_3autodrive.py
@@ -0,0 +1,70 @@
+# !/usr/bin/env python
+
+import rospy, time
+
+from linedetector import LineDetector
+from obstacledetector import ObstacleDetector
+from motordriver import MotorDriver
+
+
+class AutoDrive:
+
+    def __init__(self):
+        rospy.init_node('xycar_driver')
+        self.line_detector = LineDetector('/usb_cam/image_raw')
+        self.obstacle_detector = ObstacleDetector('/ultrasonic')
+        self.driver = MotorDriver('/xycar_motor_msg')
+        self.cnt = 0
+
+    def trace(self):
+        obs_l, obs_m, obs_r = self.obstacle_detector.get_distance()
+        line_l, line_r, fix_left, fix_right, check = self.line_detector.detect_lines()
+        self.line_detector.show_images(line_l, line_r)
+        angle = self.steer(line_l, line_r, fix_left, fix_right, check)
+        speed = self.accelerate(angle, obs_l, obs_m, obs_r, check)
+        self.driver.drive(angle + 90, speed + 90)
+
+    def steer(self, left, right, fix_l, fix_r, ck):
+        if ck:
+            fix_mid = (fix_l + fix_r) // 2
+            mid = (left + right) // 2
+            angle = -(fix_mid - mid)
+            if left > 170:
+                angle = -20
+            if abs(angle) < 15:
+                angle = 0
+                if left < fix_l - 10:
+                    angle += 5
+                elif left > fix_l + 10:
+                    angle -= 5
+        else:
+            angle = 0
+        return angle
+
+    def accelerate(self, angle, left, mid, right, ck):
+        if ck:
+            if angle == 0:
+                speed = 40
+            elif abs(angle) <= 5:
+                speed = 35
+            else:
+                speed = 35
+            self.cnt += 1
+            if self.cnt > 3000 and min(left, mid, right) < 50:
+                speed = 0
+        else:
+            speed = 0
+        return speed
+
+    def exit(self):
+        print('finished')
+
+
+if __name__ == '__main__':
+    car = AutoDrive()
+    time.sleep(3)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        car.trace()
+        rate.sleep()
+    rospy.on_shutdown(car.exit)
---
+++ assn2/12_3linedetectir.py
@@ -0,0 +1,225 @@
+# !/usr/bin/env python
+import rospy
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
+class LineDetector:
+
+    def __init__(self, topic):
+        # Initialize various class-defined attributes, and then...
+        self.left, self.right, self.fix_left, self.fix_right = -1, -1, -1, -1
+        self.roi_vertical_pos = 285
+        self.scan_height = 20
+        self.image_width = 640
+        self.scan_width = 260
+        self.area_width = 20
+        self.area_height = 10
+        self.row_begin = (self.scan_height - self.area_height) // 2
+        self.row_end = self.row_begin + self.area_height
+        self.cam_img = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
+        self.mask = np.zeros(shape=(self.scan_height, self.image_width),
+                             dtype=np.uint8)
+        self.edge = np.zeros(shape=(self.scan_height, self.image_width),
+                             dtype=np.uint8)
+        self.bridge = CvBridge()
+        rospy.Subscriber(topic, Image, self.conv_image)
+        # test
+        self.cnt = 0
+        self.check = False
+
+    def conv_image(self, data):
+        self.cam_img = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        v = self.roi_vertical_pos
+        roi = self.cam_img[v:v + self.scan_height, :]
+
+        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        avg_value = np.average(hsv[:, :, 2])
+        value_threshold = avg_value * 1.3
+        lbound = np.array([0, 0, value_threshold], dtype=np.uint8)
+        ubound = np.array([100, 255, 255], dtype=np.uint8)
+        self.mask = cv2.inRange(hsv, lbound, ubound)
+
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        edge = cv2.Canny(blur, 10, 100)
+        self.edge = cv2.cvtColor(edge, cv2.COLOR_GRAY2BGR)
+
+    def detect_lines(self):
+        if self.cnt < 30:
+            lmid = self.scan_width
+            rmid = self.image_width - lmid
+
+            pixel_cnt_threshold = 0.3 * self.area_width * self.area_height
+
+            self.cam_img = cv2.rectangle(self.cam_img, (0, self.roi_vertical_pos),
+                                         (self.image_width - 1, self.roi_vertical_pos + self.scan_height),
+                                         (255, 0, 0), 3)
+
+            hsv2 = cv2.cvtColor(self.edge, cv2.COLOR_BGR2HSV)
+
+            avg_value = np.average(hsv2[:, :, 2])
+            value_threshold = avg_value * 1.0
+
+            lbound_1 = np.array([0, 0, value_threshold], dtype=np.uint8)
+            ubound_1 = np.array([255, 128, 255], dtype=np.uint8)
+
+            bin_1 = cv2.inRange(hsv2, lbound_1, ubound_1)
+
+            left_start = -1
+            for l in range(lmid, self.area_width, -1):
+                area = bin_1[self.row_begin:self.row_end, l: l + self.area_width]
+                if cv2.countNonZero(area) > 1:
+                    left_start = l - self.area_width -10
+                    break
+
+            right_start = -1
+            for r in range(rmid, self.image_width - self.area_width):
+                area = bin_1[self.row_begin:self.row_end, r - self.area_width:r]
+                if cv2.countNonZero(area) > 1:
+                    right_start = r + self.area_width
+                    break
+
+            if right_start != -1 and left_start != -1 and self.fix_left != -1 and self.fix_right != -1:
+                self.cnt += 1
+
+            if left_start != -1:
+                for l in range(left_start, lmid):
+                    area = self.mask[self.row_begin:self.row_end, l: l + self.area_width]
+                    if cv2.countNonZero(area) > pixel_cnt_threshold:
+                        print(l)
+                        if self.cnt < 15 or self.fix_left == -1:
+                            self.fix_left = l
+                        else:
+                            if abs(self.fix_left - l) <= 5:
+                                self.fix_left = l
+                    break
+
+            if right_start != -1:
+                for r in range(right_start, rmid, -1):
+                    area = self.mask[self.row_begin:self.row_end, r - self.area_width: r]
+                    if cv2.countNonZero(area) > pixel_cnt_threshold:
+                        print(r)
+                        if self.cnt < 15 or self.fix_left == -1:
+                            self.fix_right = r
+                        else:
+                            if abs(self.fix_right - r) <= 5:
+                                self.fix_right = r
+                    break
+            if self.fix_right != -1 and self.fix_left == -1:
+                self.fix_left = 660 - self.fix_right
+            elif self.fix_left != -1 and self.fix_right == -1:
+                self.fix_right = 660 - self.fix_left
+
+            print("")
+            print("fix_left:", self.fix_left, "fix_right:", self.fix_right)
+            print("l_s", left_start, "r_S", right_start)
+            if self.cnt == 30:
+                self.check = True
+            return self.left, self.right, self.fix_left, self.fix_right, self.check
+
+        else:
+            lmid = self.scan_width
+            rmid = self.image_width - lmid
+
+            pixel_cnt_threshold = 0.3 * self.area_width * self.area_height
+
+            self.cam_img = cv2.rectangle(self.cam_img, (0, self.roi_vertical_pos),
+                                         (self.image_width - 1, self.roi_vertical_pos + self.scan_height),
+                                         (255, 0, 0), 3)
+
+            hsv2 = cv2.cvtColor(self.edge, cv2.COLOR_BGR2HSV)
+
+            avg_value = np.average(hsv2[:, :, 2])
+            value_threshold = avg_value * 1.0
+
+            lbound_1 = np.array([0, 0, value_threshold], dtype=np.uint8)
+            ubound_1 = np.array([255, 128, 255], dtype=np.uint8)
+
+            bin_1 = cv2.inRange(hsv2, lbound_1, ubound_1)
+
+            left_start = -1
+            for l in range(lmid, self.area_width, -1):
+                area = bin_1[self.row_begin:self.row_end, l: l + self.area_width]
+                if cv2.countNonZero(area) > 1:
+                    left_start = l - self.area_width
+                    break
+
+            right_start = -1
+            for r in range(rmid, self.image_width - self.area_width):
+                area = bin_1[self.row_begin:self.row_end, r - self.area_width:r]
+                if cv2.countNonZero(area) > 1:
+                    right_start = r + self.area_width
+                    break
+
+            left = -1
+            if left_start != -1:
+                for l in range(left_start, lmid):
+                    area = self.mask[self.row_begin:self.row_end, l: l + self.area_width]
+                    if cv2.countNonZero(area) > pixel_cnt_threshold:
+                        left = l
+                        break
+
+            right = -1
+            if right_start != -1:
+                for r in range(right_start, rmid, -1):
+                    area = self.mask[self.row_begin:self.row_end, r - self.area_width: r]
+                    if cv2.countNonZero(area) > pixel_cnt_threshold:
+                        right = r
+                        break
+
+            if left == -1 and right == -1:
+                return self.left, self.right, self.fix_left, self.fix_right, self.check
+
+            if self.cnt < 50:
+                self.right = self.fix_right
+                self.left = self.fix_left
+                self.cnt += 1
+                return self.left, self.right, self.fix_left, self.fix_right, self.check
+
+            if abs(self.right - right) > abs(self.right - right_start):
+                right = right_start
+
+            if abs(self.left - left) > abs(self.left - left_start):
+                left = left_start
+
+            if abs(self.fix_left - left) <= 15:
+                if abs(self.fix_right - right) > 15:
+                    self.right = self.fix_right + (self.fix_left - left)
+                    self.left = left
+                else:
+                    self.right = right
+                    self.left = left
+
+            elif abs(self.fix_right - right) <= 15:
+                if abs(self.fix_left - left) > 15:
+                    self.right = right
+                    self.left = self.fix_left + (self.fix_right - right)
+                else:
+                    self.right = right
+                    self.left = left
+
+            elif self.fix_left - left < -20:
+                if self.fix_right - right >= -15:
+                    self.left = left
+                    self.right = self.fix_right + (self.fix_left - left)
+                else:
+                    self.right = right
+                    self.left = left
+
+            elif self.fix_left - left > 20:
+                if self.fix_right - right <= 15:
+                    self.right = self.fix_right + (self.fix_left - left)
+                else:
+                    self.right = right
+                    self.left = left
+
+            # Return positions of left and right lines detected.
+            return self.left, self.right, self.fix_left, self.fix_right, self.check
+
+    def show_images(self, left, right):
+        # Display images for debugging purposes;
+        # do not forget to call cv2.waitKey().
+        pass
